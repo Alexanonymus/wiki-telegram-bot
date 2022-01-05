0a8a7a8ae9987227c0f2a96d04ee611b83d6b620
Alexanonymus <91850234+Alexanonymus@users.noreply.github.com>
2022-01-06 00:12:57 +0500

    added settings

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from telegram.bot import Bot
+from telegram.ext import Updater,Dispatcher, CommandHandler, CallbackContext
+from telegram.update import Update
+import settings
 
-bot = Bot(token='5004960896:AAHdBx7XQ0Nju_vcputVTKd2hG-WD4uBhzo')
-print(bot.get_me())
+# bot = Bot(token='5004960896:AAHdBx7XQ0Nju_vcputVTKd2hG-WD4uBhzo')
+# print(bot.get_me())
+updater = Updater(token=settings.TELEGRAM_TOKEN)
 
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Salom')
+    context.bot.send_message(chat_id = update.message.chat_id, text = 'Salom yana bir bor')
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+
+updater.start_polling()
+updater.idle()
