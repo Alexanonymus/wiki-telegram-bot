6d25606675d7a71b62eba24258993b0a14384e48
Alexanonymus <91850234+Alexanonymus@users.noreply.github.com>
2022-01-06 17:32:28 +0500

    accepted

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from telegram.bot import Bot
-from telegram.ext import Updater,Dispatcher, CommandHandler, CallbackContext
+from telegram.ext import Updater,Dispatcher, CommandHandler, CallbackContext,MessageHandler
+from telegram.ext.filters import Filters
 from telegram.update import Update
+import requests
 import settings
 
 # bot = Bot(token='5004960896:AAHdBx7XQ0Nju_vcputVTKd2hG-WD4uBhzo')
@@ -8,11 +10,35 @@ import settings
 updater = Updater(token=settings.TELEGRAM_TOKEN)
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Salom')
-    context.bot.send_message(chat_id = update.message.chat_id, text = 'Salom yana bir bor')
+    update.message.reply_text('Assalomu alaykum! Vikipediya botga xush kelibsiz! Biror maʼlumot izlash uchun /search "kalit so`z" yozing. Masalan /search Uzbekistan')
+
+def search(update: Update, context: CallbackContext):
+    args = context.args
+    if len(args) == 0:
+        update.message.reply_text('Hech bo`lmas nimadir kiriting. Malalan /search Uzbekistan')
+    else:
+        search_text = ' '.join(args)
+        response = requests.get('https://uz.wikipedia.org/w/api.php',{
+            'action':'opensearch',
+            'search':search_text,
+            'limit':1,
+            'namespace':0,
+            'format':'json',
+        })
+        result = response.json()
+        link = result[3]
+        if len(link):
+            update.message\
+                .reply_text('Sizning so`rovingiz bo`yicha havola: ' + link[0])
+        else:
+
+            update.message\
+                .reply_text('Sizning so`rovingiz bo`yicha hech narsa topilmadi :(')
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('search', search))
+dispatcher.add_handler(MessageHandler(Filters.all, start))
 
 updater.start_polling()
 updater.idle()
